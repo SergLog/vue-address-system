62626348ee92c2a54543d94563ef097bf80ed17b
Sergey Loginov <the.loginov@gmail.com>
2019-02-27 14:49:04 +0500

    Added the header and tabs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="header">
+  <img src="/src/assets/logo.png" style="width: 80px">  
+  <span>Адресная система отдела картографии УИТС г. Сургута</span>
+</div > 
+    <div class="navigation">
+    <b-tabs content-class="mt-3">
+      <b-tab title="Справочник городов" active>I'm the first fading tab</b-tab>
+      <b-tab title="Справочник поселков">I'm the second tab content</b-tab>
+      <b-tab title="Справочник улиц">Disabled tab!</b-tab>
+  </b-tabs>
+   </div>
   </div>
 </template>
 
@@ -31,30 +26,18 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.header {
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 35px;
+    /* background-color: #87CEEB; */
+  }
+  .navigation {
+    margin-right : 50px;
+    margin-left : 50px;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+span {
+    font-size: 35px;
+    margin-left: 15px;
 }
 </style>
